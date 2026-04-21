a398a58686b448babcc248c8af60e0c3c4edf63d
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2026-04-21 20:09:17 +0530

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,63 @@
+#
+#include <iostream>
+using namespace std;
+
+#define MAX 5
+
+class Queue {
+    int arr[MAX];
+    int front, rear;
+
+public:
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    // Enqueue (Insert)
+    void enqueue(int x) {
+        if (rear == MAX - 1) {
+            cout << "Queue Overflow\n";
+            return;
+        }
+        if (front == -1) front = 0;
+        arr[++rear] = x;
+        cout << x << " inserted\n";
+    }
+
+    // Dequeue (Delete)
+    void dequeue() {
+        if (front == -1 || front > rear) {
+            cout << "Queue Underflow\n";
+            return;
+        }
+        cout << arr[front++] << " removed\n";
+    }
+
+    // Display queue
+    void display() {
+        if (front == -1 || front > rear) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        cout << "Queue elements: ";
+        for (int i = front; i <= rear; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    q.display();
+
+    q.dequeue();
+    q.display();
+
+    return 0;
+}

937dfddc9745559ff1d83ce4ae6ed95bfa241c1f
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2026-05-04 19:45:45 +0530

    Update sliding_window.cpp

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -1,1 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    int sum = 0;
+    for(int i = 0; i < k; i++)
+        sum += arr[i];
+
+    int maxSum = sum;
+
+    for(int i = k; i < n; i++) {
+        sum += arr[i];
+        sum -= arr[i - k];
+        maxSum = max(maxSum, sum);
+    }
+
+    cout << maxSum;
+    return 0;
+}

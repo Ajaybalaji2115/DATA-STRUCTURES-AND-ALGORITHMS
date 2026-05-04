c5ba2fec185cea0f7c5137049d34b187ebceaf1f
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2026-05-04 19:44:07 +0530

    Update Stack-2.cpp

--- Stack-2.cpp
+++ Stack-2.cpp
@@ -1,1 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top element: " << st.top() << endl;
+
+    st.pop(); // removes top element
+
+    cout << "Top after pop: " << st.top() << endl;
+
+    cout << "Stack size: " << st.size() << endl;
+
+    if(st.empty())
+        cout << "Stack is empty\n";
+    else
+        cout << "Stack is not empty\n";
+
+    return 0;
+}

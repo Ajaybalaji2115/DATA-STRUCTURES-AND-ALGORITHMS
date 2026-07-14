74130ac34949b327795e06ca86af47d7dc519721
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2026-07-14 09:46:57 +0530

    Create sliding_window_2.cpp

---
+++ sliding_window_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSumSubarray(vector<int>& arr, int k) {
+    int n = arr.size();
+
+    int windowSum = 0;
+
+    // First window
+    for (int i = 0; i < k; i++)
+        windowSum += arr[i];
+
+    int maxSum = windowSum;
+
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i];
+        windowSum -= arr[i - k];
+        maxSum = max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
+
+int main() {
+    vector<int> arr = {2,1,5,1,3,2};
+    int k = 3;
+
+    cout << maxSumSubarray(arr, k);
+
+    return 0;
+}
